8cd602aa22de103e7e3af52b862d2d349f71b6df
Borshch Anatolii <a.borshch@betinvest.com>
2018-02-04 01:28:38 +0200

    Build

--- src/modules/Cart/reducer.js
+++ src/modules/Cart/reducer.js
@@ -7,21 +7,18 @@ export default function cartReducer(state = [], action = {}) {
       let existingIndex = findProductIndex(state, action.payload.id);
       if (existingIndex !== -1) {
         state[existingIndex].units += 1;
-        //return state.concat([]);
         return [...state];
       }
-      // return state.concat(action.payload);
-      //debugger;
       state.push(action.payload);
       return [...state];
 
     case UPDATE_ITEM_UNITS:
       let existingItemIndex = findProductIndex(state, action.payload.id);
-      if (state[existingItemIndex].units === 0 && action.payload.units === -1) {
-        break;
+
+      if (state[existingItemIndex].units === 1 && action.payload.units === -1) {
+        return [...state.slice(0, existingItemIndex), ...state.slice(existingItemIndex + 1)];
       }
       state[existingItemIndex].units += action.payload.units;
-      //return state.concat([]);
       return [...state];
 
     case DELETE_FROM_CART:
